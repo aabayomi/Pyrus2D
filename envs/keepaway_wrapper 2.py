2193c967bc97d5ff6634b13d400ef750316a797b
aabayomi <odunayo@rocketmail.com>
2024-01-05 20:26:34 -0600

    improved passing implementation

---
+++ envs/keepaway_wrapper 2.py
@@ -0,0 +1,61 @@
+# Using local gym
+import sys
+import os
+
+current_file_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_file_path + "/../../")
+
+import dowel
+from dowel import logger, tabular
+from garage.misc.prog_bar_counter import ProgBarCounter
+import time
+from absl import logging
+from env import KeepawayEnv
+import gym
+from gym.utils import seeding
+from envs.ma_gym.envs.utils.observation_space import MultiAgentObservationSpace
+import numpy as np
+
+
+class KeepawayWrapper(KeepawayEnv):
+    def __init__(self, centralized, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reward_range = (-np.inf, np.inf)
+        self.viewer = None
+        self.action_space = gym.spaces.Discrete(self.n_actions)
+        self.agent_obs_dim = np.sum(
+            [
+                np.prod(self.get_obs_move_feats_size()),
+                np.prod(self.get_obs_enemy_feats_size()),
+                np.prod(self.get_obs_ally_feats_size()),
+                np.prod(self.get_obs_own_feats_size()),
+            ]
+        )
+        self.observation_space_low = [0] * self.agent_obs_dim
+        self.observation_space_high = [1] * self.agent_obs_dim
+        self.observation_space = gym.spaces.Box(
+            low=np.array(self.observation_space_low),
+            high=np.array(self.observation_space_high),
+        )
+        self.centralized = centralized
+        if centralized:
+            self.observation_space = gym.spaces.Box(
+                low=np.array(self.observation_space_low * self.n_agents),
+                high=np.array(self.observation_space_high * self.n_agents),
+            )
+        self.pickleable = False
+
+        def reset(self):
+            obses = super().reset()[0]
+            if not self.centralized:
+                return obses
+            else:
+                return np.concatenate(obses)
+
+        def step(self, actions):
+            reward, terminated, info = super().step(actions)
+            if not self.centralized:
+                return super().get_obs(), [reward] * self.n_agents, \
+                        [terminated] * self.n_agents, info 
+            else:
+                return np.concatenate(super().get_obs()), reward, terminated, info
---
+++ test_handcoded_agent 2.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import, division, print_function
+
+import time
+from absl import logging
+logging.set_verbosity(logging.DEBUG)
+from env import KeepawayEnv
+from experiments.policies.handcoded_agent import HandcodedPolicy
+
+
+def main():
+    env = KeepawayEnv()
+    episodes = 20
+    # print("Training episodes")
+    # print("launching game")
+    env._launch_game()
+    agents = env.num_keepers
+    policy = HandcodedPolicy()
+
+    for e in range(episodes):
+        print(f"Episode {e}")
+        env.reset()
+        terminated = False
+        episode_reward = 0
+        env.start()
+
+        while not terminated:
+            obs = env.get_obs()
+            actions, agent_infos = policy.get_actions(obs,greedy=True)
+            print("actions ", actions)
+            reward, terminated, info = env.step(actions)
+            time.sleep(0.15)
+            episode_reward += reward
+
+    print("closing game")
+    env.close()
+
+if __name__ == "__main__":
+    main()
