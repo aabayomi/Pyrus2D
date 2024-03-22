ccfd996b9474b5ddd5c1ec584b0c4eb738064222
aabayomi <odunayo@rocketmail.com>
2024-03-22 18:28:16 -0500

    some fix in argument

--- keepaway/examples/training_agent.py
+++ keepaway/examples/training_agent.py
@@ -10,7 +10,7 @@ from keepaway.envs.policies.handcoded_agent import HandcodedPolicy
 from keepaway.config.game_config import get_config
 
 from absl import flags, app
-FLAGS = flags.FLAGS
+
 
 
 # Edit the configuration path if needed(this is the default path)
@@ -33,7 +33,7 @@ def setup_agent(policy):
     return agent_config
 
 
-def train_agent(env_config, agent_config, nepisode, nsteps):
+def train_agent(env_configs, agent_config, nepisode, nsteps):
 
     if agent_config.get('policy') == 'random':
         policy = RandomPolicy()
@@ -44,27 +44,45 @@ def train_agent(env_config, agent_config, nepisode, nsteps):
     else:
         raise ValueError(f"Unknown policy: {agent_config.get('policy')}")
 
-    env = KeepawayEnv(env_config)
-    env._launch_game()
+    print(f"Training agent with policy: {env_configs}")
+    env = KeepawayEnv(env_configs)
+    env.start()
 
-    for e in range(nepisode):
-        env.reset()
-        terminated = False
-        episode_reward = 0
-        env.start()
-        while not terminated:
-            obs = env.get_obs()
-            actions, agent_infos = policy.get_actions(obs, greedy=True)
-            reward, terminated, info = env.step(actions)
-            episode_reward += reward
-    print("closing game")
+    # for e in range(nepisode):
+    #     env.reset()
+    #     terminated = False
+    #     episode_reward = 0
+    #     env.start()
+    #     while not terminated:
+    #         obs = env.get_obs()
+    #         actions, agent_infos = policy.get_actions(obs, greedy=True)
+    #         reward, terminated, info = env.step(actions)
+    #         episode_reward += reward
+    # print("closing game")
     # env.close()
+    pass
+    
+def run(args):
+    agent_configs = load_agent_config(agent_config_path) 
+    env_config = args.gc
+    agent_config = agent_configs.get(args.policy, {})
 
+    if not env_config:
+        raise ValueError(f"Unknown environment configuration: {args.gc}")
+    if not agent_config:
+        raise ValueError(f"Unknown agent configuration: {args.policy}")
 
-def main():
-    # Setup argparse
+    environment = setup_environment(env_config)
+    agent = setup_agent(agent_config)
+
+    # print(f"Training agent with configuration: {agent}")
+    # print(f"Training agent with environment configuration: {environment}")
+
+    train_agent(environment, agent, args.nepisode, args.nsteps)
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train an agent with specific configurations.')
-    parser.add_argument('--game_config', type=str, default='3v2',
+    parser.add_argument('--gc', type=str, default='3v2',
                         help='Configuration to use, defines the number of keepers e.g., "3v2", "4v3", or "5v4"')
     parser.add_argument('--num_timesteps', type=int, default=int(2e6),
                         help='Number of timesteps to run for.')
@@ -76,24 +94,4 @@ def main():
                         help='Policy to use, e.g., "handcoded", "always-hold", "random"')
 
     args = parser.parse_args()
-
-    agent_configs = load_agent_config(agent_config_path)  
-
-    env_config = args.game_config
-    agent_config = agent_configs.get(args.policy, {})
-
-    if not env_config:
-        raise ValueError(f"Unknown environment configuration: {args.game_config}")
-    if not agent_config:
-        raise ValueError(f"Unknown agent configuration: {args.policy}")
-
-    # Setup environment and agent using the selected configurations
-    environment = setup_environment(env_config)
-    print(f"Environment configuration: {environment}")
-    agent = setup_agent(agent_config)
-
-    # Train the agent
-    train_agent(environment, agent, args.nepisode, args.nsteps)
-
-if __name__ == '__main__':
-    main()
+    run(args)
